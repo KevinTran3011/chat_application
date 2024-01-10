156c3dd068be45ea75aae0e2362abee1ccf9b867
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-01-10 10:17:31 +0700

    fixed the code for the positioning of the messages

--- src/components/chatRoom/chatRoom.component.jsx
+++ src/components/chatRoom/chatRoom.component.jsx
@@ -33,7 +33,7 @@ const ChatRoom = () => {
         </div>
         <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
           <ChatWindow
-            currentUserId={userData?.uid} // Changed from userData?.id to userData?.uid
+            currentUserId={userData?.uid}
             targetUserId={targetUserId}
             targetUserName={targetUserName}
             userName={userData?.userName}

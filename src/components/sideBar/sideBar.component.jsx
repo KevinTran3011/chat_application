d65ef948c8688768cdc45d98ca2c42fd91dfda3b
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-01-10 16:10:05 +0700

    added the sign out function

--- src/components/sideBar/sideBar.component.jsx
+++ src/components/sideBar/sideBar.component.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
 import { getDocs, collection } from "firebase/firestore";
-import { db } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { Link } from "react-router-dom";
 import ChatContacts from "../chatContacts/chatContacts.component";
 import InputComponent from "../Input/input.component";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
 import LanguageIcon from "@mui/icons-material/Language";
-
+import { useNavigate } from "react-router-dom";
 const SideBar = ({ onSelectUser }) => {
   const [users, setUsers] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchUsers = async () => {
       const usersCollection = await getDocs(collection(db, "users"));
@@ -22,6 +22,16 @@ const SideBar = ({ onSelectUser }) => {
 
     fetchUsers();
   }, []);
+
+  const signOutUser = async () => {
+    try {
+      await auth.signOut();
+      console.log("User signed out successfully");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error while attempting to sign out user: ${err.message}`);
+    }
+  };
 
   return (
     <div className="sideBar_container">
@@ -54,18 +64,18 @@ const SideBar = ({ onSelectUser }) => {
         </ul>
       </div>
       <div className="sideBar_footer">
-        {/* <div className="sideBar_footer--language">
-          <LanguageIcon className="sideBar_language--icon" />
-          <div className="footer_text">Language</div>
-        </div> */}
-
         <Link to="/:userId/settings">
           <div className="sideBar_footer--settings">
             <SettingsIcon className="sideBar_settings--icon" />
             <div className="settings_text">Settings</div>
           </div>
         </Link>
-        <div className="signOut_button">
+        <div
+          className="signOut_button"
+          onClick={() => {
+            signOutUser();
+          }}
+        >
           <LogoutIcon className="signOut_icon" />
           <div className="signOut_text">Sign Out</div>
         </div>

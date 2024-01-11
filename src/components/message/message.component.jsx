64e952b10cc09b8ac8ef9ae45d7207ed735a69c2
KevinTran3011 <tuankiettran30112000@gmail.com>
2024-01-11 23:44:03 +0700

    added the function so that the sennder avatar and name is displayed with the message

--- src/components/message/message.component.jsx
+++ src/components/message/message.component.jsx
@@ -1,8 +1,29 @@
 /* eslint-disable react/prop-types */
-
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Message = ({ message, currentUserId }) => {
+  const [senderData, setSenderData] = useState(null);
+  const userData = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    const fetchSenderData = async () => {
+      try {
+        const senderDocRef = doc(db, "users", message.userId);
+        const senderDocSnap = await getDoc(senderDocRef);
+        if (senderDocSnap.exists()) {
+          setSenderData(senderDocSnap.data());
+        }
+      } catch (error) {
+        console.error("Error fetching sender data:", error);
+      }
+    };
+
+    fetchSenderData();
+  }, [message.userId]);
   return (
     <div
       className={
@@ -11,7 +32,29 @@ const Message = ({ message, currentUserId }) => {
           : "message_container--receiver"
       }
     >
-      {message.text}
+      {senderData && (
+        <>
+          <div>
+            {senderData.avatar ? (
+              <img
+                src={senderData.avatar}
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "50%",
+                  marginRight: "8px",
+                }}
+                alt="Sender Avatar"
+              />
+            ) : (
+              <AccountCircleIcon style={{ fontSize: "30px" }} />
+            )}
+          </div>
+          <div>
+            <strong>{senderData.userName}</strong>: {message.text}
+          </div>
+        </>
+      )}
     </div>
   );
 };
